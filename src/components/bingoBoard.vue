888ecafcde9320971c69e0189d48136bd78189ef
Sergio <sergioa.orozcoo@gmail.com>
2023-07-19 11:52:18 -0500

    Add Bingo functionality

---
+++ src/components/bingoBoard.vue
@@ -0,0 +1,170 @@
+<script>
+export default {
+  data() {
+    return {
+      defLetter: 'B',
+      defNumber: '0',
+      selectedTags: [],
+      BingoInformation: {
+        "B": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        "I": [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+        "N": [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
+        "G": [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+        "O": [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75],
+      }
+    }
+  },
+  methods: {
+    randomCalc(steps) {
+      let item = Math.random() * steps;
+        return Math.floor(item) + 1
+    },
+
+    startBingo() {
+      let newNumber = this.randomCalc(75);
+      while (this.selectedTags.includes(newNumber)) {
+        newNumber = this.randomCalc(75);
+      }
+      this.defNumber = newNumber;
+
+      let letter = '';
+      if (this.defNumber >= 1 && this.defNumber <= 15) {
+        letter = 'B';
+      } else if (this.defNumber >= 16 && this.defNumber <= 30) {
+        letter = 'I';
+      } else if (this.defNumber >= 31 && this.defNumber <= 45) {
+        letter = 'N';
+      } else if (this.defNumber >= 46 && this.defNumber <= 60) {
+        letter = 'G';
+      } else {
+        letter = 'O';
+      }
+      this.defLetter = letter;
+      this.isNumberSelected(newNumber);
+    },
+
+    isNumberSelected(number) {
+      if(this.defNumber = number) {
+        this.selectedTags.push(number)
+      }
+    },
+
+    isBoardSelected(number) {
+      return this.selectedTags.includes(number)
+    }
+
+  }
+}
+
+</script>
+
+<template>
+  <div class="wrapper flex flex-col justify-between gap-y-10">
+    <section class="mt-12">
+      <h1 class="text-center text-slate-300 text-7xl font-bold">Vingo</h1>
+    </section>
+    <main>
+      <section class="flex flex-col gap-y-8 letters">
+        <ul class="flex gap-x-4 justify-center">
+          <li class="text-7xl font-bold">{{ defLetter }}</li>
+          <li class="text-7xl font-bold">{{ defNumber }}</li>
+        </ul>
+        <button @click="startBingo"
+          class="bg-amber-500 hover:bg-amber-400 transition-colors duration-300 text-black rounded-lg font-bold px-10 text-2xl py-4">Sortear
+          Números</button>
+      </section>
+      <section class="mt-10">
+        <div class="flex flex-col board">
+          <ul>
+            <li>
+              <p class="key">B</p>
+              <p v-for="numbers in BingoInformation.B" :class="{selected : isBoardSelected(numbers)}" class="numbers">{{ numbers }}</p>
+            </li>
+            <li>
+              <p class="key">I</p>
+              <p v-for="numbers in BingoInformation.I" :class="{selected : isBoardSelected(numbers)}" class="numbers">{{ numbers }}</p>
+            </li>
+            <li>
+              <p class="key">N</p>
+              <p v-for="numbers in BingoInformation.N" :class="{selected : isBoardSelected(numbers)}" class="numbers">{{ numbers }}</p>
+            </li>
+            <li>
+              <p class="key">G</p>
+              <p v-for="numbers in BingoInformation.G" :class="{selected : isBoardSelected(numbers)}" class="numbers">{{ numbers }}</p>
+            </li>
+            <li>
+              <p class="key">O</p>
+              <p v-for="numbers in BingoInformation.O" :class="{selected : isBoardSelected(numbers)}" class="numbers">{{ numbers }}</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.selected {
+  background-color: rgb(245, 188, 66) !important;
+  color: black;
+}
+.board {
+  // clip: auto;
+  border: 1px solid black;
+  margin: auto;
+  width: fit-content;
+  border-radius: 10px;
+  overflow: clip;
+
+  li {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    gap: 0;
+  }
+
+  .key {
+    color: black;
+    font-weight: bold;
+    width: 50px;
+    height: 50px;
+    background-color: hsl(0, 0%, 60%);
+    border: 1px solid black;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .numbers {
+    border-collapse: collapse;
+    align-items: center;
+    background: rgb(70, 70, 70);
+    border: 1px solid black;
+    display: flex;
+    height: 50px;
+    justify-content: center;
+    width: 50px;
+  }
+}
+
+.letters {
+  li {
+    align-items: center;
+    aspect-ratio: 1/1;
+    background: rgb(46, 46, 46);
+    border-radius: 12px;
+    display: flex;
+    height: 200px;
+    justify-content: center;
+    width: 200px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .letters li {
+    padding-inline: 15px;
+    height: 100px;
+    width: 100px;
+  }
+}</style>
