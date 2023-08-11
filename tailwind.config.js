a35d7d744f38d5225d07919f90a03128c48be887
Sergio <sergioa.orozcoo@gmail.com>
2023-08-11 09:48:07 -0500

    Resolve conflicts and fix animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+<<<<<<< HEAD
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+=======
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+>>>>>>> b407548... Add Bingo functionality
   theme: {
     extend: {},
   },
